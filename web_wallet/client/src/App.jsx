07277a0bbfb7a177ded30e51b096ee2e34c3a3d4
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-05-26 23:05:01 +0100

    chnaging folder structure and setting up tailwind

---
+++ web_wallet/client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="text-3xl text-underline font-extrabold text-orange-600">Hello men</h1>
+    </>
+  )
+}
+
+export default App
