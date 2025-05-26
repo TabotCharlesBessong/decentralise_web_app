b0ae84a6e5e86a915385e862fc253acd8ab01a38
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-05-26 23:28:04 +0100

    basic file and folder structure and created application structure as well

--- web_wallet/client/src/App.jsx
+++ web_wallet/client/src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import { Footer, Navbar, Services, Transactions } from "./components";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => (
+  <div className="min-h-screen">
+    <div className="gradient-bg-welcome">
+      <Navbar />
+      {/* <Welcome /> */}
+    </div>
+    <Services />
+    <Transactions />
+    <Footer />
+  </div>
+);
 
-  return (
-    <>
-      <h1 className="text-3xl text-underline font-extrabold text-orange-600">Hello men</h1>
-    </>
-  )
-}
-
-export default App
+export default App;
